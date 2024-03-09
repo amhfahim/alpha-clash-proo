c9de11c2481df5878c8ec2f577cf6a8dca32fee2
amhfahim <starfahim2018@gmail.com>
2024-03-10 03:43:04 +0600

    javascript added

---
+++ script/alpha-clash.js
@@ -0,0 +1,15 @@
+// function play(){
+//    const homeSection= document.getElementById('home')
+// //    console.log(homeScreen.classList);
+// homeSection.classList.add(
+//     'hidden'
+// )
+// const playGround=document.getElementById('play-ground');
+// // console.log(playGround.classList);
+// playGround.classList.remove('hidden')
+// }
+
+function play(){
+    hideElement('home')
+    showElement('play-ground')
+}
